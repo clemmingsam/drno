2fc05b04497f23fad817bc3fba2794c19e995550
clemmingsam <clemmingsam@gmail.com>
2019-05-27 17:09:37 -0500

    Added dll imports

---
+++ drno/Libraries/Keyboard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace drno.Libraries
+{
+    class Keyboard
+    {
+        // Dll imports below
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idhook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        // Class object declarations below
+        private LowLevelKeyboardProc keyboardProc;
+        private IntPtr hookId = IntPtr.Zero;
+
+        Keyboard()
+        {
+            
+        }
+    }
+}
