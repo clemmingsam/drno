37c02726f9285c36bf8a10b83cf82534dd2b1125
clemmingsam <clemmingsam@gmail.com>
2019-05-27 17:50:34 -0500

    Added setHook/unHook/callBack

--- drno/Libraries/Keyboard.cs
+++ drno/Libraries/Keyboard.cs
@@ -19,6 +19,9 @@ namespace drno.Libraries
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
         // Class object declarations below
         private LowLevelKeyboardProc keyboardProc;
         private IntPtr hookId = IntPtr.Zero;
@@ -26,6 +29,35 @@ namespace drno.Libraries
         Keyboard()
         {
             
+        }
+
+        public IntPtr setHook(LowLevelKeyboardProc keyboardProc)
+        {
+            const int WH_KEYBOARD_LL = 13;
+
+            using (Process currentProcess = Process.GetCurrentProcess())
+            using (ProcessModule currentModule = currentProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, keyboardProc, GetModuleHandle(currentModule.ModuleName), 0);
+            }
+        }
+
+        public void unHook()
+        {
+            UnhookWindowsHookEx(hookId);
+        }
+
+        public IntPtr callBack(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            const int WM_KEYDOWN = 0x0100;
+            const int WM_SYSKEYDOWN = 0x0104;
+
+            if (nCode >= 0 && wParam == (IntPtr) WM_KEYDOWN || wParam == (IntPtr) WM_SYSKEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+            }
+
+            return CallNextHookEx(hookId, nCode, wParam, lParam);
         }
     }
 }
